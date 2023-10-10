a928e7c461063d29c574b3c94e693195383bdba4
ektogod <114094990+ektogod@users.noreply.github.com>
2023-10-11 00:03:59 +0300

    Add files via upload

---
+++ laba-1-windows/childProccess.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+int main(int argc, char** argv)
+{
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	if (!CreateProcess(
+		L"D:\\c++\\signalWindows\\x64\\Debug\\signalWindows.exe",
+		NULL,
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&si,
+		&pi))
+	{
+		printf("CreateProcess failed (%d).\n", GetLastError());
+	}
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+	return 0;
+}
---
+++ laba-1-windows/signalWindows.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <stdlib.h>
+#include <signal.h>
+#include<cstdio>
+#include<Windows.h>
+
+void SignalHandler(int signal)
+{
+    printf("The process was finished by a signal (%d) ", signal);
+    _exit(3);
+}
+
+int main()
+{
+    typedef void (*SignalHandlerPointer)(int);
+
+    SignalHandlerPointer previousHandler;
+    previousHandler = signal(SIGINT, SignalHandler);
+
+    while (true) {
+        printf("I'm working...\n");
+        Sleep(1000);
+    }
+
+    abort();
+}
